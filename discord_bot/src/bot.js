05a2897fc6c3ba1aac6baaa8f6ec60b155051361
Hachi Metsu <13114199adarsh@gmail.com>
2022-08-21 18:39:01 +0000

    done..

--- discord_bot/src/bot.js
+++ discord_bot/src/bot.js
@@ -1,8 +1,18 @@
+console.log('In bot.js .....(/)');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages
+
+    ] });
 client.commands = new Collection();
+
 
 require(path.join(__dirname, 'handleEvents.js'))(client);
 client.handleEvents();
+
 client.login(process.env.TOKEN);
--- discord_bot/src/handleEvents.js
+++ discord_bot/src/handleEvents.js
@@ -1,21 +1,22 @@
+console.log('In handleEvents.js ...(/)');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = (client) => {
+    console.log('Loading commands ...(/)');
     const commandsPath = path.join(__dirname, 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+    for (const file of fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))) {
+        const command = require(path.join(commandsPath, file));
         client.commands.set(command.data.name, command);
     }
+    console.log('done...');
+
 
     client.handleEvents = async () => {
+        console.log('Initialising Events ...(/)');
         const eventsPath = path.join(__dirname, 'events');
-        const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-        for (const file of eventFiles) {
-            const filePath = path.join(eventsPath, file);
-            const event = require(filePath);
+        for (const file of fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'))) {
+            const event = require(path.join(eventsPath, file));
             if (event.once) {
                 client.once(event.name, (...args) => event.execute(...args, client));
             } else {
